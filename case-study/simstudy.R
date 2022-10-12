6deaac3abe78c8a226ea503c7c2f6a0041b57c5c
Marta Bofill Roig <marta.bofillroig@meduniwien.ac.at>
2022-10-12 14:58:42 +0200

    sim-res

--- case-study/simstudy.R
+++ case-study/simstudy.R
@@ -58,12 +58,11 @@ mean_arm2 = 72.3/3.5
 # Simulations
 ##########################################
 
-
-# nsim=100000
-nsim=10
+nsim=100000
+# nsim=10
 
 set.seed(4561)
-df_res = data.frame(rt=c(0),r1=c(0),r2=c(0),mu0=c(0),mu1=c(0),mu2=c(0),N=c(0),alloc=c("one"),arm="a1",H0=F)
+df_res = data.frame(rt_a1=c(0),rt_a2=c(0),r1=c(0),r2=c(0),mu0=c(0),mu1=c(0),mu2=c(0),N=c(0),alloc=c("one"),H0=F)
 i=1
 
 ##########################################
@@ -111,14 +110,21 @@ y2_t1e <- y %>%
   select(reject_h0) 
 e2_t1e <- sum(y2_t1e)/nsim
 
-df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
-             mu0=mean_control,mu1=7,mu2=7,
-             N=N,alloc="one",arm="a1",H0=F)
-df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
-             mu0=mean_control,mu1=7,mu2=7,
-             N=N,alloc="one",arm="a2",H0=F)
 
-i=i+2
+df_res[i,]=c(rt_a1=e1_t1e,rt_a2=e2_t1e, r1=N1/N,r2=N2/N,
+             mu0=mean_control,mu1=7,mu2=7,
+             N=N,alloc="one",H0=F) 
+
+i=i+1
+
+# df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=7,mu2=7,
+#              N=N,alloc="one",arm="a1",H0=F)
+# df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=7,mu2=7,
+#              N=N,alloc="one",arm="a2",H0=F)
+# 
+# i=i+2
 ##########################################
 # optimal allocation
 y_opt = rdply(nsim,
@@ -141,14 +147,21 @@ y2_t1e <- y_opt %>%
   select(reject_h0) 
 e2_t1e <- sum(y2_t1e)/nsim
 
-df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
-             mu0=mean_control,mu1=7,mu2=7,
-             N=N,alloc="opt",arm="a1",H0=F)
-df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
-             mu0=mean_control,mu1=7,mu2=7,
-             N=N,alloc="opt",arm="a2",H0=F)
 
-i=i+2
+df_res[i,]=c(rt_a1=e1_t1e,rt_a2=e2_t1e, r1=N1/N,r2=N2/N,
+             mu0=mean_control,mu1=7,mu2=7,
+             N=N,alloc="opt",H0=F)
+
+i=i+1
+
+# df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=7,mu2=7,
+#              N=N,alloc="opt",arm="a1",H0=F)
+# df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=7,mu2=7,
+#              N=N,alloc="opt",arm="a2",H0=F)
+# 
+# i=i+2
 
 ##########################################
 # sqrt k allocation
@@ -174,14 +187,20 @@ y2_t1e <- y_sqrt %>%
 e2_t1e <- sum(y2_t1e)/nsim
 
 
-df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
+df_res[i,]=c(rt_a1=e1_t1e,rt_a2=e2_t1e,r1=N1/N,r2=N2/N,
              mu0=mean_control,mu1=7,mu2=7,
-             N=N,alloc="sqrt",arm="a1",H0=F)
-df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
-             mu0=mean_control,mu1=7,mu2=7,
-             N=N,alloc="sqrt",arm="a2",H0=F)
+             N=N,alloc="sqrt",H0=F)
 
-i=i+2
+i=i+1
+
+# df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=7,mu2=7,
+#              N=N,alloc="sqrt",arm="a1",H0=F)
+# df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=7,mu2=7,
+#              N=N,alloc="sqrt",arm="a2",H0=F)
+# 
+# i=i+2
 
 list_res_H1 = list(y,y_opt,y_sqrt)
 
@@ -212,14 +231,21 @@ y2_t1e <- y %>%
   select(reject_h0) 
 e2_t1e <- sum(y2_t1e)/nsim
 
-df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
+df_res[i,]=c(rt_a1=e1_t1e,rt_a2=e2_t1e,r1=N1/N,r2=N2/N,
              mu0=mean_control,mu1=mean_control,mu2=mean_control,
-             N=N,alloc="one",arm="a1",H0=T)
-df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
-             mu0=mean_control,mu1=mean_control,mu2=mean_control,
-             N=N,alloc="one",arm="a2",H0=T)
+             N=N,alloc="one",H0=T)
 
-i=i+2
+i=i+1
+
+
+# df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=mean_control,mu2=mean_control,
+#              N=N,alloc="one",arm="a1",H0=T)
+# df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=mean_control,mu2=mean_control,
+#              N=N,alloc="one",arm="a2",H0=T)
+# 
+# i=i+2
 
 ##########################################
 # optimal allocation
@@ -243,14 +269,286 @@ y2_t1e <- y_opt %>%
   select(reject_h0) 
 e2_t1e <- sum(y2_t1e)/nsim
 
-df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
-             mu0=mean_control,mu1=7,mu2=7,
-             N=N,alloc="opt",arm="a1",H0=T)
-df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
-             mu0=mean_control,mu1=7,mu2=7,
-             N=N,alloc="opt",arm="a2",H0=T)
 
-i=i+2
+df_res[i,]=c(rt_a1=e1_t1e,rt_a2=e2_t1e,r1=N1/N,r2=N2/N,
+             mu0=mean_control,mu1=mean_control,mu2=mean_control,
+             N=N,alloc="opt",H0=T)
+
+i=i+1
+
+
+# df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=7,mu2=7,
+#              N=N,alloc="opt",arm="a1",H0=T)
+# df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=7,mu2=7,
+#              N=N,alloc="opt",arm="a2",H0=T)
+# 
+# i=i+2
+
+##########################################
+# sqrt k allocation
+
+y_sqrt = rdply(nsim,
+               do.call(rbind.data.frame,
+                       models_cc(data = sim_designs(r1=N1/N,r2=N2/N,
+                                                    mu0=mean_control,mu1=mean_control,mu2=mean_control,
+                                                    N=N,alloc="sqrt")$data
+                       )
+               )
+) 
+
+# t1e / power
+y1_t1e <- y_sqrt %>% 
+  filter(arm == "a1") %>%
+  select(reject_h0)  
+e1_t1e <- sum(y1_t1e)/nsim
+
+y2_t1e <- y_sqrt %>% 
+  filter(arm == "a2") %>%
+  select(reject_h0) 
+e2_t1e <- sum(y2_t1e)/nsim
+
+df_res[i,]=c(rt_a1=e1_t1e,rt_a2=e2_t1e,r1=N1/N,r2=N2/N,
+             mu0=mean_control,mu1=mean_control,mu2=mean_control,
+             N=N,alloc="sqrt",H0=T)
+
+i=i+1
+
+# df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=mean_control,mu2=mean_control,
+#              N=N,alloc="sqrt",arm="a1",H0=T)
+# df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=mean_control,mu2=mean_control,
+#              N=N,alloc="sqrt",arm="a2",H0=T)
+
+df_res
+list_res_H0 = list(y,y_opt,y_sqrt)
+
+##########################################
+# save.image("C:/Users/mbofi/Dropbox/CeMSIIS/GitHub/Allocation/case-study/simstudy_results.R.RData") #local
+save.image("~/GitHub/Allocation/case-study/simstudy_results_part1.RData") #server
+
+##########################################
+##########################################
+
+i=7
+
+N = 80
+N1 = round(N/4)
+N2 = round(N-N1)
+c(N1,N2,N-N1-N2)
+
+# design 2: two-period design 
+db2_one=sim_designs(r1=N1/N,r2=N2/N,mu0=mean_control,mu1=mean_arm1,mu2=mean_arm2,N=N,alloc="one")
+db2_sqrt=sim_designs(r1=N1/N,r2=N2/N,mu0=mean_control,mu1=mean_arm1,mu2=mean_arm2,N=N,alloc="sqrt")
+db2_opt=sim_designs(r1=N1/N,r2=N2/N,mu0=mean_control,mu1=mean_arm1,mu2=mean_arm2,N=N,alloc="opt")
+
+db2_one_ss <- data.frame(arms=c("A1","A2","C"),db2_one$ss, c(sum(db2_one$ss[1,]),sum(db2_one$ss[2,]),sum(db2_one$ss[3,])))
+db2_sqrt_ss <- data.frame(arms=c("A1","A2","C"), db2_sqrt$ss, c(sum(db2_sqrt$ss[1,]),sum(db2_sqrt$ss[2,]),sum(db2_sqrt$ss[3,])))
+db2_opt_ss <- data.frame(arms=c("A1","A2","C"), db2_opt$ss, c(sum(db2_opt$ss[1,]),sum(db2_opt$ss[2,]),sum(db2_opt$ss[3,])))
+
+knitr::kable(db2_one_ss, format = "markdown", caption = c("Sample sizes per period and arm (1:1)"), col.names = c("Arms", "Period 1","Period 2","Period 3", "Total per arm"))
+knitr::kable(db2_sqrt_ss, format = "markdown", caption = c("Sample sizes per period and arm (sqrt(k)-rule)"), col.names = c("Arms","Period 1","Period 2","Period 3", "Total per arm"))
+knitr::kable(db2_opt_ss, format = "markdown", caption = c("Sample sizes per period and arm (optimal allocations)"), col.names = c("Arms","Period 1","Period 2","Period 3", "Total per arm"))
+
+
+##########################################
+# Power comparison
+##########################################
+# 1:1 allocation
+y = rdply(nsim,
+          do.call(rbind.data.frame,
+                  models_cc(data = sim_designs(r1=N1/N,r2=N2/N,
+                                               mu0=mean_control,mu1=7,mu2=7,
+                                               N=N,alloc="one")$data
+                  )
+          )
+) 
+
+head(y)
+
+# t1e / power
+y1_t1e <- y %>% 
+  filter(arm == "a1") %>%
+  select(reject_h0)  
+e1_t1e <- sum(y1_t1e)/nsim
+
+y2_t1e <- y %>% 
+  filter(arm == "a2") %>%
+  select(reject_h0) 
+e2_t1e <- sum(y2_t1e)/nsim
+
+df_res[i,]=c(rt_a1=e1_t1e,rt_a2=e2_t1e,r1=N1/N,r2=N2/N,
+             mu0=mean_control,mu1=7,mu2=7,
+             N=N,alloc="one",H0=F)
+
+i=i+1
+
+
+# df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=7,mu2=7,
+#              N=N,alloc="one",arm="a1",H0=F)
+# df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
+#                mu0=mean_control,mu1=7,mu2=7,
+#                N=N,alloc="one",arm="a2",H0=F)
+# 
+# i=i+2
+##########################################
+# optimal allocation
+y_opt = rdply(nsim,
+              do.call(rbind.data.frame,
+                      models_cc(data = sim_designs(r1=N1/N,r2=N2/N,
+                                                   mu0=mean_control,mu1=7,mu2=7,
+                                                   N=N,alloc="opt")$data
+                      )
+              )
+) 
+
+# t1e / power
+y1_t1e <- y_opt %>% 
+  filter(arm == "a1") %>%
+  select(reject_h0)  
+e1_t1e <- sum(y1_t1e)/nsim
+
+y2_t1e <- y_opt %>% 
+  filter(arm == "a2") %>%
+  select(reject_h0) 
+e2_t1e <- sum(y2_t1e)/nsim
+
+df_res[i,]=c(rt_a1=e1_t1e,rt_a2=e2_t1e,r1=N1/N,r2=N2/N,
+             mu0=mean_control,mu1=7,mu2=7,
+             N=N,alloc="opt",H0=F)
+
+i=i+1
+
+# df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=7,mu2=7,
+#              N=N,alloc="opt",arm="a1",H0=F)
+# df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
+#                mu0=mean_control,mu1=7,mu2=7,
+#                N=N,alloc="opt",arm="a2",H0=F)
+# 
+# i=i+2
+
+##########################################
+# sqrt k allocation
+
+y_sqrt = rdply(nsim,
+               do.call(rbind.data.frame,
+                       models_cc(data = sim_designs(r1=N1/N,r2=N2/N,
+                                                    mu0=mean_control,mu1=7,mu2=7,
+                                                    N=N,alloc="sqrt")$data
+                       )
+               )
+) 
+
+# t1e / power
+y1_t1e <- y_sqrt %>% 
+  filter(arm == "a1") %>%
+  select(reject_h0)  
+e1_t1e <- sum(y1_t1e)/nsim
+
+y2_t1e <- y_sqrt %>% 
+  filter(arm == "a2") %>%
+  select(reject_h0) 
+e2_t1e <- sum(y2_t1e)/nsim
+
+df_res[i,]=c(rt_a1=e1_t1e,rt_a2=e2_t1e,r1=N1/N,r2=N2/N,
+             mu0=mean_control,mu1=7,mu2=7,
+             N=N,alloc="sqrt",H0=F)
+
+i=i+1
+
+# df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=7,mu2=7,
+#              N=N,alloc="sqrt",arm="a1",H0=F)
+# df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
+#                mu0=mean_control,mu1=7,mu2=7,
+#                N=N,alloc="sqrt",arm="a2",H0=F)
+# 
+# i=i+2
+
+list_restwop_H1 = list(y,y_opt,y_sqrt)
+
+##########################################
+##########################################
+# T1E comparison
+##########################################
+# 1:1 allocation
+y = rdply(nsim,
+          do.call(rbind.data.frame,
+                  models_cc(data = sim_designs(r1=N1/N,r2=N2/N,
+                                               mu0=mean_control,mu1=mean_control,mu2=mean_control,
+                                               N=N,alloc="one")$data
+                  )
+          )
+) 
+
+head(y)
+
+# t1e / power
+y1_t1e <- y %>% 
+  filter(arm == "a1") %>%
+  select(reject_h0)  
+e1_t1e <- sum(y1_t1e)/nsim
+
+y2_t1e <- y %>% 
+  filter(arm == "a2") %>%
+  select(reject_h0) 
+e2_t1e <- sum(y2_t1e)/nsim
+
+df_res[i,]=c(rt_a1=e1_t1e,rt_a2=e2_t1e,r1=N1/N,r2=N2/N,
+             mu0=mean_control,mu1=mean_control,mu2=mean_control,
+             N=N,alloc="one",H0=T)
+
+i=i+1
+
+# df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=mean_control,mu2=mean_control,
+#              N=N,alloc="one",arm="a1",H0=T)
+# df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
+#                mu0=mean_control,mu1=mean_control,mu2=mean_control,
+#                N=N,alloc="one",arm="a2",H0=T)
+# 
+# i=i+2
+
+##########################################
+# optimal allocation
+y_opt = rdply(nsim,
+              do.call(rbind.data.frame,
+                      models_cc(data = sim_designs(r1=N1/N,r2=N2/N,
+                                                   mu0=mean_control,mu1=mean_control,mu2=mean_control,
+                                                   N=N,alloc="opt")$data
+                      )
+              )
+) 
+
+# t1e / power
+y1_t1e <- y_opt %>% 
+  filter(arm == "a1") %>%
+  select(reject_h0)  
+e1_t1e <- sum(y1_t1e)/nsim
+
+y2_t1e <- y_opt %>% 
+  filter(arm == "a2") %>%
+  select(reject_h0) 
+e2_t1e <- sum(y2_t1e)/nsim
+
+df_res[i,]=c(rt_a1=e1_t1e,rt_a2=e2_t1e,r1=N1/N,r2=N2/N,
+             mu0=mean_control,mu1=mean_control,mu2=mean_control,
+             N=N,alloc="opt",H0=T)
+
+i=i+1
+
+# df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=7,mu2=7,
+#              N=N,alloc="opt",arm="a1",H0=T)
+# df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
+#                mu0=mean_control,mu1=7,mu2=7,
+#                N=N,alloc="opt",arm="a2",H0=T)
+# 
+# i=i+2
 
 ##########################################
 # sqrt k allocation
@@ -276,39 +574,28 @@ y2_t1e <- y_sqrt %>%
 e2_t1e <- sum(y2_t1e)/nsim
 
 
-df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
+df_res[i,]=c(rt_a1=e1_t1e,rt_a2=e2_t1e,r1=N1/N,r2=N2/N,
              mu0=mean_control,mu1=mean_control,mu2=mean_control,
-             N=N,alloc="sqrt",arm="a1",H0=T)
-df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
-             mu0=mean_control,mu1=mean_control,mu2=mean_control,
-             N=N,alloc="sqrt",arm="a2",H0=T)
+             N=N,alloc="sqrt",H0=T)
 
-list_res_H0 = list(y,y_opt,y_sqrt)
+i=i+1
+
+# df_res[i,]=c(rt=e1_t1e,r1=N1/N,r2=N2/N,
+#              mu0=mean_control,mu1=mean_control,mu2=mean_control,
+#              N=N,alloc="sqrt",arm="a1",H0=T)
+# df_res[i+1,]=c(rt=e2_t1e,r1=N1/N,r2=N2/N,
+#                mu0=mean_control,mu1=mean_control,mu2=mean_control,
+#                N=N,alloc="sqrt",arm="a2",H0=T)
+
+
+df_res$minrt = pmin(df_res$rt_a1,df_res$rt_a2)
+df_res
+
+
+list_restwop_H0 = list(y,y_opt,y_sqrt)
 
 ##########################################
 # save.image("C:/Users/mbofi/Dropbox/CeMSIIS/GitHub/Allocation/case-study/simstudy_results.R.RData") #local
-save.image("~/GitHub/Allocation/case-study/simstudy_results.R.RData") #server
-
-##########################################
-##########################################
-
-
-N = 80
-N1 = round(N/3)
-N2 = round(N-N1)
-c(N1,N2,N-N1-N2)
-
-# design 2: two-period design 
-db2_one=sim_designs(r1=N1/N,r2=N2/N,mu0=mean_control,mu1=mean_arm1,mu2=mean_arm2,N=N,alloc="one")
-db2_sqrt=sim_designs(r1=N1/N,r2=N2/N,mu0=mean_control,mu1=mean_arm1,mu2=mean_arm2,N=N,alloc="sqrt")
-db2_opt=sim_designs(r1=N1/N,r2=N2/N,mu0=mean_control,mu1=mean_arm1,mu2=mean_arm2,N=N,alloc="opt")
-
-db2_one_ss <- data.frame(arms=c("A1","A2","C"),db2_one$ss, c(sum(db2_one$ss[1,]),sum(db2_one$ss[2,]),sum(db2_one$ss[3,])))
-db2_sqrt_ss <- data.frame(arms=c("A1","A2","C"), db2_sqrt$ss, c(sum(db2_sqrt$ss[1,]),sum(db2_sqrt$ss[2,]),sum(db2_sqrt$ss[3,])))
-db2_opt_ss <- data.frame(arms=c("A1","A2","C"), db2_opt$ss, c(sum(db2_opt$ss[1,]),sum(db2_opt$ss[2,]),sum(db2_opt$ss[3,])))
-
-knitr::kable(db2_one_ss, format = "markdown", caption = c("Sample sizes per period and arm (1:1)"), col.names = c("Arms", "Period 1","Period 2","Period 3", "Total per arm"))
-knitr::kable(db2_sqrt_ss, format = "markdown", caption = c("Sample sizes per period and arm (sqrt(k)-rule)"), col.names = c("Arms","Period 1","Period 2","Period 3", "Total per arm"))
-knitr::kable(db2_opt_ss, format = "markdown", caption = c("Sample sizes per period and arm (optimal allocations)"), col.names = c("Arms","Period 1","Period 2","Period 3", "Total per arm"))
+save.image("~/GitHub/Allocation/case-study/simstudy_results.RData") #server
 
 
